6c5faf81580b5784e369b0172f288718287f6776
David Valles <davidtjvalles@gmail.com>
2021-04-07 19:39:13 -0700

    Shorten property name for stack.__store

--- src/stack.py
+++ src/stack.py
@@ -1,30 +1,29 @@
 class Stack:
     def __init__(self):
-        self.__private_store = []
+        self.__store = []
         self.currentIdx = 0
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.currentIdx == len(self.__private_store):
+        if self.currentIdx == len(self.__store):
             raise StopIteration
 
-        item = self.__private_store[self.currentIdx]
+        item = self.__store[self.currentIdx]
         self.currentIdx += 1
 
         return item
 
     def __len__(self):
-        return len(self.__private_store)
+        return len(self.__store)
 
     def push(self, item):
-        self.__private_store = [item, *self.__private_store]
+        self.__store = [item, *self.__store]
 
     def pop(self):
-        if len(self.__private_store) == 0:
+        if len(self.__store) == 0:
             return None
 
-        item, self.__private_store = [self.__private_store[0],
-                                      self.__private_store[1:]]
+        item, self.__store = [self.__store[0], self.__store[1:]]
         return item
