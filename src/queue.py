fc91baba2945e90fab81d8e02dbdc461c4efd5cc
David Valles <davidtjvalles@gmail.com>
2021-04-07 20:34:49 -0700

    Add queue

---
+++ src/queue.py
@@ -0,0 +1,20 @@
+from src.stack import Stack
+
+
+class Queue:
+    def __init__(self):
+        self.__in_stack = Stack()
+        self.__out_stack = Stack()
+
+    def __len__(self):
+        return len(self.__in_stack) + len(self.__out_stack)
+
+    def enqueue(self, item):
+        self.__in_stack.push(item)
+
+    def dequeue(self):
+        if len(self.__out_stack) == 0:
+            while len(self.__in_stack) > 0:
+                self.__out_stack.push(self.__in_stack.pop())
+
+        return self.__out_stack.pop()
---
+++ tests/test_queue.py
@@ -0,0 +1,36 @@
+import unittest
+from src.queue import Queue
+
+
+class TestQueue(unittest.TestCase):
+    def test_new_queue_is_empty(self):
+        queue = Queue()
+        self.assertEqual(len(queue), 0)
+
+    def test_enqueue_adds_to_queue(self):
+        queue = Queue()
+        queue.enqueue(3)
+        queue.enqueue(4)
+        self.assertEqual(len(queue), 2)
+
+    def test_dequeue_removes_from_queue(self):
+        queue = Queue()
+        queue.enqueue(3)
+        queue.enqueue(4)
+        queue.dequeue()
+        self.assertEqual(len(queue), 1)
+
+    def test_first_in_first_out(self):
+        queue = Queue()
+        queue.enqueue(3)
+        queue.enqueue(4)
+        queue.dequeue()
+        queue.enqueue(8)
+        queue.enqueue(9)
+        queue.dequeue()
+        result = queue.dequeue()
+        self.assertEqual(result, 8)
+
+    def test_dequeue_on_empty_queue(self):
+        queue = Queue()
+        self.assertIsNone(queue.dequeue())
